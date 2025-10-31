9fba4c20328b191f5232f6f2f0e4c3b904389dd9
ncq01112006 <nc.quang0111@gmail.com>
2025-10-31 15:50:12 +0700

    first commit

---
+++ backend/src/main/java/com/bookrental/book_rental_service/BookRentalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bookrental.book_rental_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRentalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRentalServiceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bookrental/book_rental_service/BookRentalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookrental.book_rental_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRentalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
