8f3641778508d9f9515b9b1f9a45d99f143be32d
ncq01112006 <nc.quang0111@gmail.com>
2025-11-02 11:25:57 +0700

    add: table jsx, table css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,27 @@
 import "./App.css";
-
+import Table from "./components/table/Table";
 function App() {
   function handleClick() {
     return;
   }
-  return <div className="container">hi</div>;
+  const columns = ["id", "title", "author", "price"];
+  const data = [
+    { id: 1, title: "Harry Potter", author: "J.K. Rowling", price: "$10" },
+    { id: 2, title: "Clean Code", author: "Robert C. Martin", price: "$15" },
+    {
+      id: 3,
+      title: "The Pragmatic Programmer",
+      author: "Andrew Hunt",
+      price: "$12",
+    },
+  ];
+
+  return (
+    <div className="container">
+      hi
+      <Table className="customer-table" columns={columns} data={data}></Table>
+    </div>
+  );
 }
 
 export default App;
